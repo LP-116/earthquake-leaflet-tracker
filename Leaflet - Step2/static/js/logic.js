aaef8956414738713e407d52b0ea573eef0b921e
Leanne <leanne_porter@hotmail.com>
2021-09-04 16:51:26 +1000

    starting bonus code

--- Leaflet - Step2/static/js/logic.js
+++ Leaflet - Step2/static/js/logic.js
@@ -1,13 +1,10 @@
-var myMap = L.map("map", {
-    center: [36.0544, -112.1401],
-    zoom: 5,
-});
+var earthquakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+var tectonicPlatesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
-
-d3.json(queryUrl).then(function(data) {
+d3.json(earthquakeUrl).then(function(data) {
     createFeatures(data.features);
   });
+
 
 function magnitudeColor(depth)  {
     switch (true) {
@@ -61,9 +58,54 @@ function createFeatures(earthquakeData) {
             return L.circleMarker(latlng);},
         style: styleDetail,
         onEachFeature: onEachFeature
-      }).addTo(myMap);
+      })
 
-    var legend = L.control({position: 'bottomright'});
+    console.log(earthquakes)
+    
+    createMap(earthquakes);
+
+    };
+
+
+
+
+function createMap(earthquakes) {
+
+    var tectonicplates = L.layerGroup();
+
+    d3.json(tectonicPlatesUrl).then(function(data) {
+        L.geoJSON(data, {
+            color: "orange"
+        }).addTo(tectonicplates)
+    });
+
+    var lightlayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+    });
+
+    var baseMaps = {
+        "Grayscale": lightlayer,
+    }
+
+    var overlayMaps = {
+        Earthquakes: earthquakes,
+        Tectonic: tectonicplates
+    };
+
+
+    var myMap = L.map("map", {
+        center: [36.0544, -112.1401],
+        zoom: 5,
+        layers:[lightlayer, earthquakes, tectonicplates]
+    });
+
+
+     var legend = L.control({position: 'bottomright'});
 
       legend.onAdd = function (map) {
       
@@ -71,7 +113,6 @@ function createFeatures(earthquakeData) {
           grades = [-10, 10, 30, 50, 70, 90],
           labels = [];
       
-          // loop through our density intervals and generate a label with a colored square for each interval
           for (var i = 0; i < grades.length; i++) {
               div.innerHTML +=
                   '<div><i style="background:' + legendColor(grades[i] + 1) + '"></i> ' + 
@@ -80,48 +121,13 @@ function createFeatures(earthquakeData) {
       
       return div;
       };
-      
+
       legend.addTo(myMap);
-   
-     
 
-      createMap(earthquakes);
-    };
-
-
-function createMap(earthquakes) {
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
 
     
 
-    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/light-v10",
-    accessToken: API_KEY
-
-
 }).addTo(myMap);
 }
-
-// var legend = L.control({position: 'bottomright'});
-
-//       legend.onAdd = function (map) {
-      
-//           var div = L.DomUtil.create('div', 'info legend'),
-//           grades = [-10, 10, 30, 50, 70, 90],
-//           labels = [];
-      
-//           // loop through our density intervals and generate a label with a colored square for each interval
-//           for (var i = 0; i < grades.length; i++) {
-//               div.innerHTML +=
-//                   '<i style="background:' + legendColor(grades[i] + 1) + '"></i> ' +
-//                   grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-//       }
-      
-//       return div;
-//       };
-      
-//       legend.addTo(myMap);
-      
